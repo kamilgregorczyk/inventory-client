5b9194a51d61f4747cb127d062282c46e6d83eda
Kamil Gregorczyk <gregorczyk@me.com>
2020-11-19 18:58:15 +0100

    Initial commit

--- client/client.go
+++ client/client.go
@@ -11,19 +11,19 @@ import (
 
 func NewInventoryClient(timeout time.Duration, url *url.URL) *InventoryClient {
 	return &InventoryClient{
-		url:  url,
-		http: &http.Client{Timeout: timeout},
+		Url:  url,
+		Http: &http.Client{Timeout: timeout},
 	}
 }
 
 type InventoryClient struct {
-	url  *url.URL
-	http *http.Client
+	Url  *url.URL
+	Http *http.Client
 }
 
-func (c *InventoryClient) GetItems() (*[]Inventory, error) {
-	fullPath := c.url.String() + "/inventory"
-	response, err := c.http.Get(fullPath)
+func (c *InventoryClient) GetItems() ([]Inventory, error) {
+	fullPath := c.Url.String() + "/inventory"
+	response, err := c.Http.Get(fullPath)
 
 	if err != nil {
 		return nil, InventoryClientError{Message: fmt.Sprintf("Failed to make a request %s", err.Error()), Url: fullPath}
@@ -45,5 +45,5 @@ func (c *InventoryClient) GetItems() (*[]Inventory, error) {
 		return nil, InventoryClientError{Message: fmt.Sprintf("Failed due to parsing error %s", err.Error()), Url: fullPath}
 	}
 
-	return &items, nil
+	return items, nil
 }
